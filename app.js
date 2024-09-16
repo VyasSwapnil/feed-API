397c6b01fbd04833715891c3e9a6aff574d2dcb1
swapnil <swapnilvyas18@gmail.com>
2024-09-16 15:10:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
     res.setHeader('Access-Control-Allow-hEADERS', 'Content-Type, Authorization');
+    next();
 })
 app.use("/feed", feedRoutes);
 app.listen(8080);
