03d6d399cea08ba66612147dc8b9c61f9e202275
swapnil <swapnilvyas18@gmail.com>
2024-09-16 14:58:20 +0530

    Added Cross-Origin

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ const express = require("express");
 const feedRoutes = require("./routes/feed");
 
 const app = express();
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-hEADERS', 'Content-Type, Authorization');
+})
 app.use("/feed", feedRoutes);
 app.listen(8080);
