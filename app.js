e4778db18062d5aa340a8507eaa3d6d6df7372a0
swapnil <swapnilvyas18@gmail.com>
2024-09-17 15:57:35 +0530

    Added body-parser for POST requests

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const feedRoutes = require("./routes/feed");
 
 const app = express();
+app.use(bodyParser.json()); 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
