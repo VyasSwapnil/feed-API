efdd1869d8385d01850fba03989feff34b907c0b
swapnil <swapnilvyas18@gmail.com>
2024-09-27 14:52:55 +0530

    Updated getPosts to fetch from DB

--- controller/feed.js
+++ controller/feed.js
@@ -2,18 +2,14 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    _id: "1",
-    posts: [
-      {
-        title: "First Post",
-        content: "This is the first post!",
-        imageUrl: "images/cat.jpg",
-        creator: { name: "Swapnil" },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then((posts) => {
+      res.status(200).json({ message: "Fetched posts.", posts: posts });
+    })
+    .catch((err) => {
+      if (!err.statusCode) err.statusCode = 500;
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
